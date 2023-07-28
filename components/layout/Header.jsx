09baef27bddc74824f132ab221cb12603ac0b357
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2023-07-28 11:19:12 +0300

    Add commit

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <div
       className={`h-[5.5rem] z-50 relative w-full ${
-        router.asPath === "/" ? "bg-transparent" : "bg-secondary !fixed"
+        router.asPath === "/" ? "bg-transparent" : "bg-secondary "
       }`}
     >
       <div className="container mx-auto text-white flex justify-between items-center h-full">
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Carousel from "../../components/Carousel";
+import Campaigns from "@/components/Campaigns";
+import MenuWrapper from "@/components/product/MenuWrapper";
+import About from "@/components/About";
+import Input from "@/components/form/Input";
+import Reservation from "@/components/Reservation";
 
 const Index = () => {
   return (
-    <div className="relative">
+    <div>
       <Carousel />
+      <Campaigns />
+      <MenuWrapper />
+      <About />
+      <Reservation />
     </div>
   );
 };
